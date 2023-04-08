79951cde13ed7a97ada1836dd0a9af71f54051fa
prinze2005-prod <97626070+prinze2005-prod@users.noreply.github.com>
2023-04-07 19:15:14 -0600

    added permission

--- scripts/runApp.sh
+++ scripts/runApp.sh
@@ -7,7 +7,7 @@ echo 'The following "npm" command runs your Node.js application'
 set -x
 npm start & # for it to run in the background
 sleep 1
-echo $! > .pidfile  # echo $! will show the scripts running in the background
+echo $! > tee .pidfile  # echo $! will show the scripts running in the background
 set +x
 
 echo 'Now you can'
